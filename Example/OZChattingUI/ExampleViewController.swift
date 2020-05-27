ddcce6dd527e748c6516c48ce2f9d83d60b2d99a
Henry Kikm <neoroman@alterant.kr>
2020-05-27 17:51:17 +0900

    [feature development] add image viewer sample

--- Example/OZChattingUI/ExampleViewController.swift
+++ Example/OZChattingUI/ExampleViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 import OZChattingUI
 import ImageViewer
 
+extension UIImageView: DisplaceableView {}
+
+struct DataItem: Equatable {
+    static func == (lhs: DataItem, rhs: DataItem) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+    let identifier: String  // custom
+    let timestamp: Int      // custom
+    
+    let imageView: UIImageView
+    let galleryItem: GalleryItem
+}
+
+
 class ExampleViewController: UIViewController {
     
     var chatViewController: OZMessagesViewController?
+
+    var galleryVC: GalleryViewController?
+    var chatImageItems: [DataItem] = []
+    var selectedImage: UIImage?
+    var imageViewerRightButton: UIButton?
+    var imageViewerLeftButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -134,6 +154,14 @@ class ExampleViewController: UIViewController {
 
 
 extension ExampleViewController: OZMessagesViewControllerDelegate {
+    func messageCellDidSetMessage(cell: OZMessageCell) {
+        // code
+    }
+    
+    func messageCellLayoutSubviews(cell: OZMessageCell) {
+        // code
+    }
+    
     func messageViewLoaded(isLoaded: Bool) {
         print("messageViewLoaded...!")
     }
@@ -154,6 +182,15 @@ extension ExampleViewController: OZMessagesViewControllerDelegate {
                     }
                 }
             }
+        }
+        else if let aCell = cell as? ImageMessageCell {
+            if aCell.message.type == .emoticon {
+                // It's emoticon
+                return
+            }
+            selectedImage = aCell.imageView.image
+            makeGalleryItemsFromAllMessages()
+            showGalleryImageViewer(identifier: aCell.message.identifier)
         }
     }
     
@@ -215,6 +252,222 @@ extension ExampleViewController: UIDocumentBrowserViewControllerDelegate {
     }
 }
 
+
+
+
+// MARK: - ImageViewer
+extension ExampleViewController {
+    // MARK: - Targets and Actions
+    @objc func backButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true) {
+            self.selectedImage = nil
+            self.galleryVC = nil
+        }
+    }
+    @objc func fileDownButtonTapped(_ sender: Any) {
+        if let gvc = galleryVC,
+            let anImage = self.selectedImage  {
+            let activityVC = UIActivityViewController(activityItems: [anImage], applicationActivities: nil)
+            gvc.present(activityVC, animated: true)
+        }
+    }
+    func makeGalleryItemsFromAllMessages() {
+        if let ccv = chatViewController {
+            for x in ccv.dataSource.data {
+                if x.type == .emoticon { continue }
+                guard let dataIndex = ccv.dataSource.data.firstIndex(of: x) else { continue }
+                
+                if x.content.hasPrefix("/") {
+                    makeGalleryItems(path: x.content, data: x)
+                }
+                else if let imgCell = ccv.collectionView.cell(at: dataIndex) as? ImageMessageCell,
+                    x.type == .image {
+
+                    makeGalleryItems(cell: imgCell)
+                }
+            }
+        }
+    }
+    func makeGalleryItems(cell: OZMessageCell) {
+        guard let imgCell = cell as? ImageMessageCell else{ return }
+
+        let imageView = imgCell.imageView
+        var galleryItem: GalleryItem!
+        
+        guard let image = imageView.image else { return }
+        galleryItem = GalleryItem.image { $0(image) }
+        
+        guard !chatImageItems.contains(where: { (a) -> Bool in
+            return a.identifier == imgCell.message.identifier
+        }) else { return }
+        chatImageItems.append(DataItem(identifier: imgCell.message.identifier, timestamp: imgCell.message.timestamp, imageView: imageView, galleryItem: galleryItem))
+        chatImageItems.sort { (a, b) -> Bool in
+            return a.timestamp < b.timestamp
+        }
+    }
+    func makeGalleryItems(path: String, data: OZMessage) {
+        guard let anImage = UIImage(contentsOfFile: path) else{ return }
+
+        var galleryItem: GalleryItem!
+        galleryItem = GalleryItem.image { $0(anImage) }
+        
+        guard !chatImageItems.contains(where: { (a) -> Bool in
+            return a.identifier == data.identifier
+        }) else { return }
+        let anImageView = UIImageView(image: anImage)
+        chatImageItems.append(DataItem(identifier: data.identifier, timestamp: data.timestamp, imageView: anImageView, galleryItem: galleryItem))
+        chatImageItems.sort { (a, b) -> Bool in
+            return a.timestamp < b.timestamp
+        }
+    }
+    func showGalleryImageViewer(identifier: String) {
+        for x in chatImageItems {
+            if x.identifier == identifier, let anIndex = chatImageItems.firstIndex(of: x) {
+                showGalleryImageViewer(index: anIndex)
+                break
+            }
+        }
+    }
+    func showGalleryImageViewer(index: Int = 0) {
+        
+        var anIndex = index
+        if anIndex == 0, chatImageItems.count > 0 {
+            anIndex = chatImageItems.count - 1
+        }
+        
+        let frame = CGRect(x: 0, y: 0, width: 200, height: 24)
+        let headerView = CounterView(frame: frame, currentIndex: anIndex, count: chatImageItems.count)
+        let footerView = CounterView(frame: frame, currentIndex: anIndex, count: chatImageItems.count)
+        
+        let galleryViewController = GalleryViewController(startIndex: anIndex, itemsDataSource: self, itemsDelegate: self, displacedViewsDataSource: self, configuration: galleryConfiguration())
+        galleryViewController.headerView = headerView
+        galleryViewController.footerView = footerView
+        galleryVC = galleryViewController
+        
+        galleryViewController.launchedCompletion = {
+            print("LAUNCHED")
+            if let aRightButton = self.imageViewerRightButton {
+                aRightButton.removeTarget(galleryViewController, action: nil, for: .allEvents)
+                aRightButton.setTitle("Share", for: .normal)
+                aRightButton.setTitleColor(.gray, for: .normal)
+                aRightButton.addTarget(self,
+                                       action: #selector(self.fileDownButtonTapped(_:)),
+                                       for: .touchUpInside)
+            }
+            if let aLeftButton = self.imageViewerLeftButton {
+                aLeftButton.center = CGPoint(x: 20, y: 20)
+                aLeftButton.removeTarget(galleryViewController, action: nil, for: .allEvents)
+                aLeftButton.setTitle("Close", for: .normal)
+                aLeftButton.setTitleColor(.gray, for: .normal)
+                aLeftButton.addTarget(self,
+                                       action: #selector(self.backButtonTapped(_:)),
+                                       for: .touchUpInside)
+                galleryViewController.view.addSubview(aLeftButton)
+            }
+        }
+        galleryViewController.closedCompletion = { print("CLOSED") }
+        galleryViewController.swipedToDismissCompletion = { print("SWIPE-DISMISSED") }
+        galleryViewController.landedPageAtIndexCompletion = { index in
+            print("LANDED AT INDEX: \(index)")
+            self.selectedImage = self.chatImageItems[index].imageView.image
+            
+            // headerView.count = self.chatImageItems.count
+            // headerView.currentIndex = index
+
+            let dateFormatterGet = DateFormatter()
+            dateFormatterGet.dateFormat = "MMM. d, h:mm a"
+
+            headerView.countLabel.text = "\(Date(timeIntervalSince1970: TimeInterval(self.chatImageItems[index].timestamp)))"
+            headerView.isHidden = false // by Henry on 2020.05.13
+            footerView.count = self.chatImageItems.count
+            footerView.currentIndex = index
+        }
+        self.presentImageGallery(galleryViewController)
+    }
+    
+    func galleryConfiguration() -> GalleryConfiguration {
+        if imageViewerRightButton == nil {
+            imageViewerRightButton = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 50, height: 50)))
+        }
+        if imageViewerLeftButton == nil {
+            imageViewerLeftButton = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 50, height: 50)))
+        }
+        return [
+            GalleryConfigurationItem.closeButtonMode(.custom(imageViewerRightButton!)),
+            GalleryConfigurationItem.deleteButtonMode(.none),
+            GalleryConfigurationItem.seeAllCloseButtonMode(.none),
+            GalleryConfigurationItem.thumbnailsButtonMode(.custom(imageViewerLeftButton!)),
+
+            GalleryConfigurationItem.pagingMode(.standard),
+            GalleryConfigurationItem.presentationStyle(.displacement),
+            GalleryConfigurationItem.hideDecorationViewsOnLaunch(false),
+            
+            GalleryConfigurationItem.swipeToDismissMode(.vertical),
+            GalleryConfigurationItem.toggleDecorationViewsBySingleTap(true),
+            GalleryConfigurationItem.activityViewByLongPress(true),
+                        
+            GalleryConfigurationItem.overlayColor(UIColor(white: 238/255, alpha: 1)),
+            GalleryConfigurationItem.overlayColorOpacity(1),
+            GalleryConfigurationItem.overlayBlurOpacity(1),
+            GalleryConfigurationItem.overlayBlurStyle(UIBlurEffect.Style.light),
+            
+            GalleryConfigurationItem.videoControlsColor(.white),
+            
+            GalleryConfigurationItem.maximumZoomScale(8),
+            GalleryConfigurationItem.swipeToDismissThresholdVelocity(300),//(500),
+            
+            GalleryConfigurationItem.doubleTapToZoomDuration(0.15),
+            
+            GalleryConfigurationItem.blurPresentDuration(0.5),
+            GalleryConfigurationItem.blurPresentDelay(0),
+            GalleryConfigurationItem.colorPresentDuration(0.25),
+            GalleryConfigurationItem.colorPresentDelay(0),
+            
+            GalleryConfigurationItem.blurDismissDuration(0.1),
+            GalleryConfigurationItem.blurDismissDelay(0.4),
+            GalleryConfigurationItem.colorDismissDuration(0.45),
+            GalleryConfigurationItem.colorDismissDelay(0),
+            
+            GalleryConfigurationItem.itemFadeDuration(0.3),
+            GalleryConfigurationItem.decorationViewsFadeDuration(0.15),
+            GalleryConfigurationItem.rotationDuration(0.15),
+            
+            GalleryConfigurationItem.displacementDuration(0.55),
+            GalleryConfigurationItem.reverseDisplacementDuration(0.25),
+            GalleryConfigurationItem.displacementTransitionStyle(.springBounce(0.7)),
+            GalleryConfigurationItem.displacementTimingCurve(.linear),
+            
+            GalleryConfigurationItem.statusBarHidden(true),
+            GalleryConfigurationItem.displacementKeepOriginalInPlace(false),
+            GalleryConfigurationItem.displacementInsetMargin(50),
+            
+            GalleryConfigurationItem.rotationMode(.applicationBased)
+        ]
+    }
+}
+
+
+extension ExampleViewController: GalleryDisplacedViewsDataSource {
+    func provideDisplacementItem(atIndex index: Int) -> DisplaceableView? {
+        return index < chatImageItems.count ? chatImageItems[index].imageView : nil
+    }
+}
+extension ExampleViewController: GalleryItemsDataSource {
+    func itemCount() -> Int {
+        return chatImageItems.count
+    }
+    func provideGalleryItem(_ index: Int) -> GalleryItem {
+        return chatImageItems[index].galleryItem
+    }
+}
+extension ExampleViewController: GalleryItemsDelegate {
+    func removeGalleryItem(at index: Int) {
+        print("remove item at \(index)")
+        let imageView = chatImageItems[index].imageView
+        imageView.removeFromSuperview()
+        chatImageItems.remove(at: index)
+    }
+}
 
 
 // MARK: - OZChattingUI choosing image, camera, file here
