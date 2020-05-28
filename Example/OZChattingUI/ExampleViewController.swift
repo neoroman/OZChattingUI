e502f1d1eaabc969fb221051a5191a8ad80739b5
Henry Kikm <neoroman@alterant.kr>
2020-05-28 12:58:57 +0900

    [feature development] add work around of draw() for OZBubbleLabel part 3

--- Example/OZChattingUI/ExampleViewController.swift
+++ Example/OZChattingUI/ExampleViewController.swift
@@ -159,15 +159,15 @@ extension ExampleViewController: OZMessagesViewControllerDelegate {
         if cell.message.type == .text {
             if let incomingCell = cell as? IncomingTextMessageCell {
                 
-                incomingCell.textLabel.layer.shadowOffset = CGSize(width: 0, height: 3)
-                incomingCell.textLabel.layer.shadowOpacity = 0.2
-                incomingCell.textLabel.layer.shadowRadius = 8
+                incomingCell.textLabel.layer.shadowOffset = CGSize(width: 2, height: 2)
+                incomingCell.textLabel.layer.shadowOpacity = 0.04
+                incomingCell.textLabel.layer.shadowRadius = 2
                 incomingCell.textLabel.layer.shadowColor = shadowColor.cgColor
                 incomingCell.textLabel.layer.shadowPath = UIBezierPath(roundedRect: incomingCell.textLabel.bounds, cornerRadius: 12).cgPath
             }
             else if let outgoingCell = cell as? OutgoingTextMessageCell {
                 
-                outgoingCell.textLabel.layer.shadowOffset = CGSize(width: 0, height: 3)
+                outgoingCell.textLabel.layer.shadowOffset = CGSize(width: 0, height: 2)
                 outgoingCell.textLabel.layer.shadowOpacity = 0.2
                 outgoingCell.textLabel.layer.shadowRadius = 8
                 outgoingCell.textLabel.layer.shadowColor = shadowColor.cgColor
@@ -178,27 +178,27 @@ extension ExampleViewController: OZMessagesViewControllerDelegate {
     }
     
     func messageCellLayoutSubviews(cell: OZMessageCell, previousMessage: OZMessage) {
-//        if cell.message.alignment == .left {
-//            switch cell.message.type {
-//            case .text:
-//                guard let incomingCell = cell as? IncomingTextMessageCell else { return }
-//                incomingCell.iconImage.isHidden = true
-//                let inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-//                incomingCell.textLabel.frame = incomingCell.bounds.inset(by: inset)
-//            case .image, .emoticon:
-//                guard let incomingCell = cell as? ImageMessageCell else { return }
-//                incomingCell.iconImage.isHidden = true
-//                let inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-//                incomingCell.imageView.frame = incomingCell.bounds.inset(by: inset)
-//            case .voice, .mp3:
-//                guard let incomingCell = cell as? AudioPlusIconMessageCell else { return }
-//                incomingCell.iconImage.isHidden = true
-//                let inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-//                incomingCell.backView.frame = incomingCell.bounds.inset(by: inset)
-//            default:
-//                print(".....\(cell.message.type), prevMsg(\(String(describing: previousMessage))).....")
-//            }
-//        }
+        if cell.message.alignment == .left {
+            switch cell.message.type {
+            case .text:
+                guard let incomingCell = cell as? IncomingTextMessageCell else { return }
+                incomingCell.iconImage.isHidden = true
+                let inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+                incomingCell.textLabel.frame = incomingCell.bounds.inset(by: inset)
+            case .image, .emoticon:
+                guard let incomingCell = cell as? ImageMessageCell else { return }
+                incomingCell.iconImage.isHidden = true
+                let inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+                incomingCell.imageView.frame = incomingCell.bounds.inset(by: inset)
+            case .voice, .mp3:
+                guard let incomingCell = cell as? AudioPlusIconMessageCell else { return }
+                incomingCell.iconImage.isHidden = true
+                let inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+                incomingCell.backView.frame = incomingCell.bounds.inset(by: inset)
+            default:
+                print(".....\(cell.message.type), prevMsg(\(String(describing: previousMessage))).....")
+            }
+        }
     }
     func messageViewLoaded(isLoaded: Bool) {
         print("messageViewLoaded...!")
--- Example/OZChattingUI/OZBubbleLabelExtension.swift
+++ Example/OZChattingUI/OZBubbleLabelExtension.swift
@@ -21,8 +21,8 @@ extension OZBubbleLabel {
         if height < 60,
             let aText = self.attributedText?.string,
             aText.isHangul() {
-            height = height - 10
-            width = width - 5
+            height = (height <= 44) ? height : height - 9
+            width = width - 2
         }
         
         let cornerRatio: CGFloat = 5.37 / 12.0
@@ -34,24 +34,24 @@ extension OZBubbleLabel {
         let notchHeight: CGFloat = 8.0 / 12.0 * radius
         let notchRadius: CGFloat = 3.0 / 12.0 * radius
         let notchCircleStartPoint = CGPoint(x: width-notchInsetX + (3.6/12.0*radius),
-                                                     y: height-notchHeight + (4.8/12.0*radius))
+                                            y: height-notchHeight + (4.8/12.0*radius))
         
         let notchCircle1stEndPoint = CGPoint(x: width - (0.8/12.0*radius),
-                                               y: height - (0.4/12.0*radius))
+                                             y: height - (0.4/12.0*radius))
         let notchCircle1stCntlPoint1 = CGPoint(x: width + (0.27/12.0*radius),
                                                y: height-notchRadius + (0.69/12.0*radius))
         let notchCircle1stCntlPoint2 = CGPoint(x: width + (0.09/12.0*radius),
                                                y: height-notchRadius + (1.94/12.0*radius))
-
+        
         let notchCircle2ndCntlPoint1 = CGPoint(x: width - (1.14/12.0*radius),
                                                y: height - (0.14/12.0*radius))
         let notchCircle2ndCntlPoint2 = CGPoint(x: width - (1.56/12.0*radius),
                                                y: height)
         let notchCircle2ndEndPoint = CGPoint(x: width-(2.0/12.0 * radius),
-                                                   y: height)
-
+                                             y: height)
+        
         let bezierPath = UIBezierPath()
-
+        
         if isIncoming {
             ///CCW
             // right-bottom corner
@@ -74,7 +74,7 @@ extension OZBubbleLabel {
             
             // left-top corner
             bezierPath.addCurve(to: CGPoint(x: notchInsetX, y: radius),
-                controlPoint1: CGPoint(x: notchInsetX + cntlRadius, y: 0),
+                                controlPoint1: CGPoint(x: notchInsetX + cntlRadius, y: 0),
                                 controlPoint2: CGPoint(x: notchInsetX, y: cntlRadius))
             
             // left wall
@@ -104,7 +104,7 @@ extension OZBubbleLabel {
             
             // left-bottom corner
             bezierPath.addCurve(to: CGPoint(x: 0, y: height-radius), controlPoint1: CGPoint(x: cntlRadius, y: height), controlPoint2: CGPoint(x: 0, y: height-cntlRadius))
-
+            
             // left wall
             bezierPath.addLine(to: CGPoint(x: 0, y: radius))
             
@@ -116,13 +116,13 @@ extension OZBubbleLabel {
             
             // right-top corner
             bezierPath.addCurve(to: CGPoint(x: width-notchInsetX, y: radius), controlPoint1: CGPoint(x: width-notchInsetX-cntlRadius, y: 0), controlPoint2: CGPoint(x: width-notchInsetX, y: cntlRadius))
-
+            
             // right wall
             bezierPath.addLine(to: CGPoint(x: width-notchInsetX, y: height - notchHeight))
-
+            
             // slope line of notch
             bezierPath.addLine(to: notchCircleStartPoint)
-                        
+            
             // bubble notch
             bezierPath.addCurve(to: notchCircle1stEndPoint,
                                 controlPoint1: notchCircle1stCntlPoint1,
@@ -131,13 +131,13 @@ extension OZBubbleLabel {
                                 controlPoint1: notchCircle2ndCntlPoint1,
                                 controlPoint2: notchCircle2ndCntlPoint2)
             
-
+            
             // bottom wall
             bezierPath.addLine(to: CGPoint(x: radius, y: height))
-
+            
             //outgoingColor.setFill()
             UIColor(red: 0.000, green: 0.746, blue: 0.718, alpha: 1.000).setFill()
-
+            
         }
         
         bezierPath.close()
@@ -145,4 +145,5 @@ extension OZBubbleLabel {
         
         super.draw(rect)
     }
+    
 }
