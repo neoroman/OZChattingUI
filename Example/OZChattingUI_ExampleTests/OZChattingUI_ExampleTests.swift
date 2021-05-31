919871b224772bf5766b6f108102d5486279f46c
Henry Kim <neoroman@gmail.com>
2021-05-31 15:32:42 +0900

    [feature] add test bundle

---
+++ Example/OZChattingUI_ExampleTests/OZChattingUI_ExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  OZChattingUI_ExampleTests.swift
+//  OZChattingUI_ExampleTests
+//
+//  Created by Henry Kim on 2021/05/31.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class OZChattingUI_ExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
